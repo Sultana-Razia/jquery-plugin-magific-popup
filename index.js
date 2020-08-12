fdd25a15c3b142109bec117a50af923f9f973969
Sultana-Razia <61369017+Sultana-Razia@users.noreply.github.com>
2020-08-12 11:56:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+$('.image-link').magnificPopup({
+        type:'image',
+        gallery:{
+            enabled:true
+        }
+    });
+});
+
+
+
+// $(".change-text h1").text("ok");
+// $(".change-text p").html("<b>YES</b>");
+// $(".add-text p").append("  it's ok");
+// $(".add-text p").prepend("it's ok  ");
